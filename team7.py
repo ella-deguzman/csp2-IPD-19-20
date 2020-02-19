a905f745043e3dc5beb1e2a4b28546cee50afe2c
ella-deguzman <elladeguzmann@gmail.com>
2020-02-19 18:34:19 +0000

    We pasted our strategy to team7.py.

--- team7.py
+++ team7.py
@@ -7,24 +7,24 @@
 ####
 
 team_name = 'The Escapees' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Copy'
+strategy_description = 'Always copy the previous output of the opponent.'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    '''Make my move based on the history with this player.
     
-    Make my move.
-    Returns 'c' or 'b'. 
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray.
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    if len(my_history)==0: # It's the first round; collude.
+        return 'c'
+    elif their_history[-1]=='b':#checks if the opponent betrayed in the previous round
+        return 'b' #copies the opponent and puts a betrayal for the next round 
+    else:
+        return 'c' # otherwise collude.
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
 
